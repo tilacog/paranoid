2a74e5362adc5a5af2e4abe96394f0055f3ff1ec
Tiago Guimaraes <tilacog@gmail.com>
2015-09-10 18:38:31 -0300

    Removed too much specific methods from base FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -91,14 +91,6 @@ class FunctionalTest(StaticLiveServerTestCase):
         # one more try, which will raise any errors if they are outstanding
         return function_with_assertion()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
-
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
             lambda b: b.find_element_by_id(element_id),
@@ -106,16 +98,6 @@ class FunctionalTest(StaticLiveServerTestCase):
                 element_id, self.browser.find_element_by_tag_name('body').text
             )
         )
-
-    def wait_to_be_logged_in(self, email):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(email, navbar.text)
-
-    def wait_to_be_logged_out(self, email):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(email, navbar.text)
 
     def create_pre_authenticated_session(self, email):
         if self.against_staging:
