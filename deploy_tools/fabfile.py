5389ba0b5c5bbb06c71018f839fb6d687e6bd110
Tiago Guimaraes <tilacog@gmail.com>
2015-09-10 17:43:38 -0300

    Started django project

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https://github.com/tilacog/ottg.git'
+REPO_URL = 'https://github.com/tilacog/paranoid.git'
 
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)
