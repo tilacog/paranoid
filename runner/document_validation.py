f0efb2c13b82c4e94ad94f30814b4254c4888eda
Tiago Guimaraes <tilacog@gmail.com>
2015-10-03 21:57:39 -0300

    Added runner module

---
+++ runner/document_validation.py
@@ -0,0 +1,68 @@
+import os.path
+
+import magic
+from django.conf import settings
+
+from audits.models import Document
+from runner.plugin_mount import PluginMount
+
+
+class DocumentTypeError(Exception):
+    pass
+
+class ValidationError(Exception):
+    pass
+
+class DocumentValidatorProvider(metaclass=PluginMount):
+    """
+    Mount point for plugins which refer to validations that can be performed.
+    Plugins implementing this reference should provide the following attributes:
+
+    Attributes
+    ----------
+    doctype : the name of the doctype handled by the validator
+
+    Methods
+    -------
+
+    validate : method to validate the file. Should return the file if ok, or
+               raise an appropriate valdiation error.
+    
+    """
+    def __init__(self, document_pk):
+        self.document_pk = document_pk
+        self.error = None
+        self.clean = False
+
+    def _check_type(self):
+        document_instance = Document.objects.get(pk=self.document_pk)
+        doc_file = os.path.join(
+            settings.MEDIA_ROOT,
+            document_instance.file.url,
+        )
+        mime = magic.from_file(doc_file, mime=True)
+        if mime != document_instance.doctype.mime:
+            raise DocumentTypeError('Wrong file type')
+
+    def validate(self, file_obj):
+        # This method should be defined in subclasses
+        raise NotImplementedError
+
+    def run(self):
+        # Validate file tipe
+        try:
+            self._check_type()
+        except DocumentTypeError as e:
+            self.error = e
+            return
+    
+        # Validate file content
+        with Document.objects.get(pk=self.document_pk).file as document_file:
+            try:
+                self.validate(document_file)
+            except ValidationError as e:
+                self.error = (e)
+                return
+        
+        # If no errors were captured, update validator status
+        self.clean = True
---
+++ runner/tests.py
@@ -0,0 +1,78 @@
+from unittest.mock import DEFAULT, Mock, patch
+
+from django.test import TestCase, override_settings
+
+from runner.document_validation import (DocumentTypeError,
+                                        DocumentValidatorProvider)
+
+
+class BaseClassTest(TestCase):
+
+    def tearDown(self):
+
+        # Reset base class registered subclasses
+        DocumentValidatorProvider.plugins = []
+
+    def test_subclasses_are_registered(self):
+        # No subclass is registered at the begining
+        self.assertEqual(DocumentValidatorProvider.plugins, [])
+
+        # Create a subclass
+        class test_class(DocumentValidatorProvider):
+            pass
+
+        # Now subclass must be registered
+        self.assertIn(test_class, DocumentValidatorProvider.plugins)
+
+
+class DocumentValidatorProviderTest(TestCase):
+
+    def setUp(self):
+        patcher = patch('runner.document_validation.Document', autospec=True)
+        self.addCleanup(patcher.stop)
+        self.mock_doc_cls = patcher.start()
+
+        self.mock_doc = self.mock_doc_cls.objects.get.return_value
+        self.mock_doc.pk = 1
+        self.mock_doc.doctype.mime = 'text/plain'
+        self.mock_doc.file.url = 'foo'
+
+    @override_settings(MEDIA_ROOT='')
+    @patch('runner.document_validation.magic', autospec=True)
+    def test_can_check_mime(self, mock_magic):
+        mock_magic.from_file.return_value = 'text/plain'
+
+        dvp = DocumentValidatorProvider(1)
+        dvp._check_type()
+
+        mock_magic.from_file.assert_called_with(
+            self.mock_doc.file.url,
+            mime=True
+        )
+    
+    @override_settings(MEDIA_ROOT='')
+    @patch('runner.document_validation.magic', autospec=True)
+    def test_can_detect_invalid_mime(self, mock_magic):
+        mock_magic.from_file.return_value = 'invalid/mime'
+
+        dvp = DocumentValidatorProvider(1)
+
+        with self.assertRaises(DocumentTypeError):
+            dvp._check_type()
+
+    @patch.multiple(
+        DocumentValidatorProvider,
+        _check_type=DEFAULT,
+        validate=DEFAULT,
+    )
+    def test_can_dispatch_validation(self, _check_type, validate): 
+        
+        # Shorter name for the document file context manager
+        doc_file_cm = self.mock_doc.file.__enter__.return_value
+
+        dvp = DocumentValidatorProvider(1)
+        dvp.run()
+        
+        _check_type.assert_called_with()
+        validate.assert_called_with(doc_file_cm)
+
