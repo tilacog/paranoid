3a337ba745416ca496f7f24952f5816e89fab69a
Tiago Guimaraes <tilacog@gmail.com>
2015-12-30 12:02:58 -0200

    Set up first squeeze page tests definitions

---
+++ functional_tests/test_squeeze.py
@@ -0,0 +1,29 @@
+from base import FunctionalTest
+
+
+class SqueezeTest(FunctionalTest):
+    "BDD tests for the squeeze page"
+
+    def setUp(self):
+        # Create base user
+
+        # Create audit models
+
+    def test_can_upload_a_sped_document_via_squeeze_page(self):
+        """Users should be able to opt-in and schedule an audit.
+        """
+        pass
+
+    def test_can_visit_a_valid_download_link(self):
+        """Valid links should point to a page with a download link.
+        """
+        pass
+
+    def test_cant_visit_an_expired_download_link(self):
+        """Expired links should point to a 'sorry' page.
+        """
+        pass
+
+
+    
+
