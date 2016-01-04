e3d99b1406348199e1c425cfe5ce754255acca01
Tiago Guimaraes <tilacog@gmail.com>
2016-01-04 14:44:00 -0200

    Add Postfix settings

--- paranoid/settings.py
+++ paranoid/settings.py
@@ -177,3 +177,13 @@ except ImportError as e:
 
 # Load local plugins
 import runner.plugins_local
+
+
+# Settings for sending email with Postfix
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'Contato Titan <titan@paranoidlabs.com.br>'
