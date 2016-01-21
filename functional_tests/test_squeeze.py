b7c9b64d22ad35b7d6d52ce7cb184826adbb7814
Tiago Guimaraes <tilacog@gmail.com>
2016-01-21 17:33:37 -0200

    Made a first sketch of the landing page

--- functional_tests/test_squeeze.py
+++ functional_tests/test_squeeze.py
@@ -21,6 +21,12 @@ class SqueezeTest(FunctionalTest):
         # Create audit models
         AuditFactory(num_doctypes=1)
 
+    def fill(self, field_id, value):
+        """Helper method to fill values on form fields."""
+        field = self.browser.find_element_by_id(field_id)
+        field.send_keys(value)
+
+
     def test_can_upload_a_sped_document_via_squeeze_page(self):
         """Users should be able to opt-in and schedule an audit.
         """
@@ -34,34 +40,26 @@ class SqueezeTest(FunctionalTest):
             self.assertIn(keyword.lower(), title.text.lower())
 
         # an intro text
-        intro_text = self.browser.find_element_by_id('id_intro_text')
-        self.assertIn('titan', intro_text.lower())
+        about_text = self.browser.find_element_by_id('id_about_text')
+        self.assertIn('titan', about_text.text.lower())
 
         # an email form
         email_form = self.browser.find_element_by_id('id_email_form')
         # audit selection radio inputs
         audit_selection = self.browser.find_elements_by_css_selector(
-            'input[type="radio"]'
-        )
+            'input[type="radio"]')
 
-        # an upload button
-        upload_button = self.browser.find_elements_by_css_selector(
-            'button[type="submit"]'
-        )
-
-        # The user will submit the form with an invalid email address
-        email_form.send_keys('blarhg@\n')
-
-        # The site returns a warning about the invalid email
-        self.wait_for_element_with_id('id_invalid_email_error')
-
-        # The user will then submit a valid form
-        email_form.send_keys('testtitan@mailinator.com\n')
+        submit_button = self.browser.find_element_by_css_selector(
+            'input[type="submit"]')
+        
+        # The user submits the form
+        self.fill('id_name_field', 'John Doe')
+        self.fill('id_email_field', 'testtitan@mailinator.com')
+        submit_button.click()
 
         # After submitting the valid form, the user is taken to a confirmation
         # page.
         self.fail('Finish the test!')
-
 
     def test_can_visit_a_valid_download_link(self):
         """Valid links should point to a page with a download link.
--- squeeze/tests.py
+++ squeeze/tests.py
@@ -13,5 +13,25 @@ class SqueezePageTest(TestCase):
     def test_view_renders_right_template(self):
         self.assertTemplateUsed(self.response, 'landing.html')
 
-    def test_response_contains_basic_elements(self):
-        self.assertContains(self.response, '<h1>')
+    def test_form_contains_basic_elements(self):
+        self.assertContains(self.response, '<h1')
+        self.assertContains(self.response, 'id="id_about_text"')
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, 'id="id_email_form"')
+        self.assertContains(self.response, 'id="id_name_field"')
+        self.assertContains(self.response, 'id="id_email_field"')
+        self.assertContains(self.response, 'type="email"')
+        self.assertContains(self.response, 'type="radio"')
+        self.assertContains(self.response, 'type="file"')
+        self.assertContains(self.response, 'type="submit"')
+
+class FormTest(TestCase):
+            
+    def test_email_field_is_required(self):
+        pass
+
+    def test_name_field_is_required(self):
+        pass
+    def test_file_upload_restrict_extensions(self):
+        pass
+
