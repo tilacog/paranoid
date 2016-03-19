3b9c744f630a01dc2152472dcae3b1e54818a0bd
Tiago Guimaraes <tilacog@gmail.com>
2016-03-19 09:46:53 -0300

    Refactor squeeze.forms

--- squeeze/forms.py
+++ squeeze/forms.py
@@ -1,16 +1,32 @@
 from django import forms
 
-CHOICES = (('1', 'First'), ('2', 'Second'))
 
-
+CHOICES = (
+    # Those should map with audits.Audit.runner_choices for all the
+    # "dump-to-excel" runners. First items must match the runner class names.
+    ('SefipToExcel', 'GFIP (arquivo .SFP)'),
+    ('EfdDump', 'EFD Contribuições'),
+    ('EcfDump', 'ECF'),
+)
 
 
 class OptInForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={
-        'id':'id_name','placeholder':'Seu nome'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'id':'id_email','placeholder':'Email'}))
-    audit = forms.ChoiceField(widget=forms.RadioSelect(
-        attrs={'id':'id_audit'}), choices=CHOICES)
-    document = forms.FileField(widget=forms.ClearableFileInput(attrs={
-        'id':'id_file'}))
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'id': 'id_name',
+            'placeholder': 'Seu nome'
+        }))
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'id': 'id_email',
+            'placeholder': 'Email'
+        }))
+    audit = forms.ChoiceField(
+        widget=forms.RadioSelect(attrs={'id': 'id_audit'}),
+        choices=CHOICES)
+    document = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={'id': 'id_file'}))
+
+    def save(self):
+        #TODO
+        pass
